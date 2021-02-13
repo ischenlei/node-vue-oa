390ae8e3c2b56e1a4283326c4cb1d9001bc6469f
陈蕾 <chenlei@techsight.cn>
2021-02-13 17:42:42 +0800

    轮播图

---
+++ admin/src/views/AdEdit.vue
@@ -0,0 +1,115 @@
+<template>
+  <div class="categoryEdit">
+    <h1>{{ id ? '编辑' : '新建' }}广告位</h1>
+    <el-form label-width="120px" @submit.native.prevent="save">
+      <el-form-item label="名称">
+        <el-input v-model="model.name"></el-input>
+      </el-form-item>
+      <el-form-item label="广告">
+        <el-button size="small" @click="model.items.push({})"><i class="el-icon-plus"></i> 添加广告</el-button>
+        <el-row type="flex" style="flex-wrap: wrap">
+          <el-col :md="24" v-for="(item,i) in model.items" :key="i">
+            <el-form-item label="跳转链接">
+              <el-input v-model="item.url"></el-input>
+            </el-form-item>
+            <el-form-item label="图片">
+              <el-upload
+                class="avatar-uploader"
+                :action="$http.defaults.baseURL + '/upload'"
+                :show-file-list="false"
+                :on-success="res => $set(item,'image', res.url)">
+                <img v-if="item.image" :src="item.image" class="avatar">
+                <i v-else class="el-icon-plus avatar-uploader-icon"></i>
+              </el-upload>
+            </el-form-item>
+            <el-form-item>
+              <el-button
+                type="danger"
+                size="small"
+                v-model="item.tips"
+                @click="model.items.splice(i, 1)">删除
+              </el-button>
+            </el-form-item>
+          </el-col>
+        </el-row>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" native-type="submit">保存</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {}
+  },
+  data() {
+    return {
+      model: {
+        items: []
+      }
+    }
+  },
+  methods: {
+    async save() {
+      // let res
+      if (this.id) {
+        //修改
+        await this.$http.put(`rest/ads/${this.id}`, this.model)
+      } else {
+        //新建
+        await this.$http.post('rest/ads', this.model)
+      }
+      await this.$router.push('/ads/list')
+      this.$message({
+        type: 'success',
+        message: '保存成功'
+      })
+
+    },
+    //
+    async fetch() {
+      const res = await this.$http.get(`rest/ads/${this.id}`)
+      this.model = Object.assign({}, this.model, res.data)
+      // this.model = {...this.model, ...res.data}
+    }
+  },
+  created() {
+    this.id && this.fetch()
+  }
+}
+</script>
+
+<style scoped>
+.avatar-uploader{
+  position: relative;
+  overflow: hidden;
+  width: 200px;
+  height: 100px;
+  margin-top: 20px;
+  border: 1px dashed #d9d9d9;
+  border-radius: 6px;
+  cursor: pointer;
+}
+
+.avatar-uploader:hover {
+  border-color: #409EFF;
+}
+
+.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 200px;
+  height: 100px;
+  line-height: 100px;
+  text-align: center;
+}
+
+.avatar {
+  width: 200px;
+  height: 100px;
+  display: block;
+}
+</style>
