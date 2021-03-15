543e1028541112a76e0331d9a22bbcba20822007
陈蕾 <chenlei@techsight.cn>
2021-03-16 00:09:14 +0800

    nav icons

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -10,6 +10,19 @@
       <div class="swiper-pagination pagination-home" slot="pagination"></div>
     </swiper>
     <!--  end of swiper  -->
+    <div class="nav-icons bg-white mt-3  text-center pt-3 text-dark-1">
+      <div class="d-flex flex-wrap">
+        <div class="nav-item mb-3" v-for="n in 4" :key="n">
+          <i class="sprite sprite-news"></i>
+          <div class="py-2">爆料站</div>
+        </div>
+      </div>
+      <div class="bg-light py-2 fs-sm d-flex jc-center ai-center">
+        <i class="sprite sprite-arrow mr-1"></i>
+        <span>收起</span>
+      </div>
+    </div>
+    <!--  end of nav icons  -->
   </div>
 </template>
 
@@ -47,6 +60,17 @@ export default {
     background-color: map-get($colors, 'white');
     &.swiper-pagination-bullet-active {
       background: map-get($colors, 'info');
+    }
+  }
+}
+.nav-icons {
+  border-top: 1px solid $border-color;
+  border-bottom: 1px solid $border-color;
+  .nav-item {
+    width: 25%;
+    border-right: 1px solid $border-color;
+    &:nth-child(4n) {
+      border-right: none;
     }
   }
 }
