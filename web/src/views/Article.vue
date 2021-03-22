648caf234b57ffaf7da3e515c40b67298e80e6b2
陈蕾 <chenlei@techsight.cn>
2021-03-22 22:23:35 +0800

    新闻详情页

---
+++ web/src/views/Article.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="page-article" v-if="model">
+    <div class="d-flex py-3 px-2 border-bottom">
+      <div class="iconfont icon-Back text-blue"></div>
+      <strong class="flex-1 text-blue pl-2">
+        {{model.title}}
+      </strong>
+      <div class="text-grey fs-xs">
+        2020-3-21
+      </div>
+    </div>
+    <div v-html="model.body" class="px-3 body fs-lg"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    id: {
+      required: true
+    }
+  },
+  data() {
+    return {
+      model: null
+    }
+  },
+  methods: {
+    async fetch() {
+      const res = await this.$http.get(`/articles/${this.id}`)
+      this.model = res.data
+    }
+  },
+  created() {
+    this.fetch()
+  }
+}
+</script>
+<style lang="scss">
+.page-article {
+  .icon-Back {
+    font-size: 1.692rem;
+  }
+  .body {
+    img {
+      width: 100%;
+      height: auto;
+    }
+    iframe {
+      width: 100%;
+      height: auto;
+    }
+  }
+}
+</style>
